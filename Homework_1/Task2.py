8b38d71994b79d6ed974fd3625fa34296cf42951
ZinovevMY <mehan@list.ru>
2023-07-18 12:19:14 +0300

    Done

--- Homework_1/Task2.py
+++ Homework_1/Task2.py
@@ -2,4 +2,8 @@
 # у пользователя количество рядов.
 
 line_count = int(input("Введите количество рядов: "))
-
+delemiter = " "
+counter = 1
+for n in range(line_count):
+    print(delemiter*(line_count - n) + "*"*counter)
+    counter += 2
