efb9954c279429f288e1734d685c92e7ef57cad3
Mikhail <mehan@list.ru>
2023-07-19 21:02:57 +0300

    Done

--- Homework_1/Task3.py
+++ Homework_1/Task3.py
@@ -1,14 +1,13 @@
 # Выведите в консоль таблицу умножения от 2х2 до 9х10 как на школьной тетрадке.
 
 
-def printresult(number_1, number_2):
-    print(number_1, "x", number_2, "=", number_1 * number_2, end='\t')
+def mult_table(start_number: int, finish_number: int):
+    for n in range(2, 11):
+        for m in range(start_number, finish_number + 1):
+            print(m, "x", n, "=", m * n, end='\t')
+        print(" ")
 
 
-first_num = 2
-second_num = 2
-while second_num < 11:
-    while first_num < 6:
-        printresult(first_num, second_num)
-        first_num += 1
-    second_num += 1
+mult_table(2, 5)
+print('\n')
+mult_table(6, 9)
