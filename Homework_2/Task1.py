17d0963cdd59d55ef3bbd70121a6893959ea30d3
ZinovevMY <mehan@list.ru>
2023-07-22 09:13:19 +0300

    Done

--- Homework_2/Task1.py
+++ Homework_2/Task1.py
@@ -31,10 +31,8 @@ def accrue_dividend(balance):
 
 
 def luxury_tax(balance):
-    tax = 0
-    if balance > LUXURY_BALANCE:
-        tax = int(balance / 10)
-        print(f"Начислен налог на роскошь в размере {tax} у.е.")
+    tax = int(balance / 10)
+    print(f"Начислен налог на роскошь в размере {tax} у.е.")
     return tax
 
 
@@ -44,7 +42,7 @@ def increase_balance(balance: int, operation_counter: int):
         if increase_summ % 50 != 0:
             print("Указана неверная сумма!")
         else:
-            if operation_counter % 3 == 0:
+            if operation_counter > 0 and operation_counter % 3 == 0:
                 dividend = accrue_dividend(balance)
                 print(f"Начислены проценты в размере {dividend} у.е.")
                 increase_summ += dividend
@@ -58,18 +56,21 @@ def decrease_balance(balance: int, operation_counter: int):
         decrease_summ = int(input("Укажите сумму для снятия (кратно 50): "))
         if decrease_summ % 50 != 0:
             print("Указана неверная сумма!")
-        elif decrease_summ > balance:
-            print("Недостаточно средств на счете!")
         else:
             percent = decrease_percent(balance)
             if percent < MIN_PERCENT:
                 percent = 30
             elif percent > MAX_PERCENT:
                 percent = 600
-        if operation_counter % 3 == 0:
+            decrease_summ += percent
+        if decrease_summ > balance:
+            print(f"Недостаточно средств на счете! Снимаемая сумма с учетом комиссии составляет {decrease_summ} у.е.")
+            decrease_summ = 0
+            return decrease_summ
+        if operation_counter > 0 and operation_counter % 3 == 0:
             dividend = accrue_dividend(balance)
             print(f"Начислены проценты в размере {dividend} у.е.")
-        return decrease_summ + percent + dividend
+        return decrease_summ - dividend
 
 
 def show_balance(balance: int):
@@ -85,15 +86,17 @@ while True:
         case 1:
             show_balance(balance)
         case 2:
-            if luxury_tax(balance) > 0:
-                balance -= luxury_tax(balance)
+            if balance > LUXURY_BALANCE:
+                tax = luxury_tax(balance)
+                balance -= tax
                 show_balance(balance)
             balance += increase_balance(balance, op_counter)
             op_counter += 1
             show_balance(balance)
         case 3:
-            if luxury_tax(balance) > 0:
-                balance -= luxury_tax(balance)
+            if balance > LUXURY_BALANCE:
+                tax = luxury_tax(balance)
+                balance -= tax
                 show_balance(balance)
             balance -= decrease_balance(balance, op_counter)
             op_counter += 1
