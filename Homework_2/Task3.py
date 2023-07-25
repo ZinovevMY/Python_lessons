22a5f61f0dec97ab799531418786c60e158a38b2
ZinovevMY <mehan@list.ru>
2023-07-25 19:34:46 +0300

    start lcm

--- Homework_2/Task3.py
+++ Homework_2/Task3.py
@@ -9,6 +9,17 @@ def gcb(num1: int, num2: int) -> int:
     return gcb(num2 % num1, num1)
 
 
+def lcm(num1: int, num2: int) -> int:
+    if num1 < num2:
+        for i in range(2, num2 + 1):
+            if (num1 * i) % num2 == 0:
+                return num1 * i
+    else:
+        for i in range(2, num1 + 1):
+            if (num2 * i) % num1 == 0:
+                return num2 * i
+
+
 def mult_fractions(fract1: list, fract2: list) -> str:
     numerators_mult = int(fract1[0]) * int(fract2[0])
     denominators_mult = int(fract1[1]) * int(fract2[1])
@@ -17,6 +28,10 @@ def mult_fractions(fract1: list, fract2: list) -> str:
         numerators_mult = numerators_mult // divisor
         denominators_mult = denominators_mult // divisor
     return str(numerators_mult) + "/" + str(denominators_mult)
+
+
+def summ_fractions(fract1: list, fract2: list) -> str:
+    pass
 
 
 fraction1 = input("Введите первое дробное число: ")
