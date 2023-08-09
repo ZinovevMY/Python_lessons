39b17f4ced821489636775d9616939d22f31799a
Mikhail <mehan@list.ru>
2023-08-09 14:51:44 +0300

    Done

--- Homework_5/Task7.py
+++ Homework_5/Task7.py
@@ -5,4 +5,24 @@
 # правило: «число является простым, если делится
 # нацело только на единицу и на себя».
 
+
+def is_prime(num):
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+def prime_generator(num):
+    counter = 0
+    number = 2
+    while counter < num:
+        if is_prime(number):
+            yield number
+            counter += 1
+        number += 1
+
+
 nums = int(input("Введите количество простых чисел: "))
+
+print(*prime_generator(nums))
