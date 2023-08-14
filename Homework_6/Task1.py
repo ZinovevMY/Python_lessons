a39a72f42b5d6e5026d706e7268f272c2f2a48d5
ZinovevMY <mehan@list.ru>
2023-08-14 15:52:19 +0300

    Done

---
+++ Homework_6/Task1.py
@@ -0,0 +1,6 @@
+# Вспомните какие модули вы уже проходили на курсе.
+# Создайте файл, в котором вы импортируете встроенные в модуль функции под псевдонимами. (3-7 строк импорта).
+
+from sys import path as p
+from random import randint as ri
+from math import sqrt as s
