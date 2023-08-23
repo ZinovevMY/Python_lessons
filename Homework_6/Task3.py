74a11ad1d1eb29fda49ffc860ec718d670aaabc3
ZinovevMY <mehan@list.ru>
2023-08-23 19:10:30 +0300

    Done

--- Homework_6/Task3.py
+++ Homework_6/Task3.py
@@ -2,3 +2,18 @@
 # Добавьте возможность запуска функции “угадайки” из модуля в командной строке терминала.
 # Строка должна принимать от 1 до 3 аргументов: параметры вызова функции.
 # Для преобразования строковых аргументов командной строки в числовые параметры используйте генераторное выражение.
+
+import argparse
+import somemodule as sm
+
+parser = argparse.ArgumentParser(description="Получаем параметры функции")
+parser.add_argument('bottom', type=int, help="Нижний диапазон генератора")
+parser.add_argument('top', type=int, help="Верхний диапазон генератора")
+parser.add_argument('attempts', type=int, help="Количество попыток")
+args = parser.parse_args()
+
+is_guessed = sm.guess_number(args.bottom, args.top, args.attempts)
+if is_guessed:
+    print("Угадал")
+else:
+    print("Не угадал")
