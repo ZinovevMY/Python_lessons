2a240c33a8c6cb17bf4d62bf842075c0880b7be4
ZinovevMY <mehan@list.ru>
2023-08-26 10:40:46 +0300

    Done

--- Homework_6/mistery_module.py
+++ Homework_6/mistery_module.py
@@ -2,15 +2,13 @@
 # Функция получает на вход загадку, список с возможными вариантами отгадок и количество попыток на угадывание.
 # Программа возвращает номер попытки, с которой была отгадана загадка или ноль, если попытки исчерпаны.
 
-def mistery(mistery_text: str, clues_list: list[str], attempts_num: int) -> int:
-    counter = 1
-    print(f'Отгадайте загадку с {attempts_num} попыток!')
-    print(mistery_text)
-    while counter <= attempts_num:
-        clue = input("Введите отгадку: ")
-        for item in clues_list:
-            if item.lower() == clue.lower():
-                return counter
+def mistery(mistery_text: str, clue: str, clues_list: list[str], attempts_num: int) -> int:
+    counter = 0
+    print(f'Отгадайте загадку "{mistery_text}" с {attempts_num} попыток!')
+    while counter < attempts_num:
+        for i in range(0, attempts_num):
+            if clues_list[i].lower() == clue.lower():
+                return counter + 1
             else:
                 counter += 1
     else:
