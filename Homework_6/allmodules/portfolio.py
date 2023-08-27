30dcbd28111ebfc222b1f58fef8be8e07b1c5886
ZinovevMY <mehan@list.ru>
2023-08-27 13:07:40 +0300

    all done

--- Homework_6/allmodules/portfolio.py
+++ Homework_6/allmodules/portfolio.py
@@ -35,6 +35,8 @@
 # MSFT
 
 __all__ = ['calculate_portfolio_value', 'calculate_portfolio_return', 'get_most_profitable_stock']
+
+
 def calculate_portfolio_value(stocks: dict, prices: dict) -> float:
     summary = 0
     for k, v in stocks.items():
