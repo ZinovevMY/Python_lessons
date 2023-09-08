e176b9628469cbb84d3443a98516bed64f752e35
ZinovevMY <mehan@list.ru>
2023-09-08 19:56:45 +0300

    v.1

--- Homework_7/Task6.py
+++ Homework_7/Task6.py
@@ -5,15 +5,16 @@
 # ✔ Существующие файлы не должны удаляться/изменяться в случае совпадения имён.
 
 from pathlib import Path
+import Task1 as t1
 import Task2 as t2
 import Task3 as t3
 import Task4 as t4
 
 
 def files_in_folder(dir_name: str, file_name: str):
-    full_path = Path(dir_name + file_name)
-    if not dir_name.exists():
-        Path.mkdir(dir_name)
+    full_path = Path(dir_name)
+    if not full_path.exists():
+        Path.mkdir(full_path)
     else:
         cnt = 1
         while full_path.exists():
@@ -23,4 +24,10 @@ def files_in_folder(dir_name: str, file_name: str):
             full_path.rename(new_path)
             cnt += 1
 
+    t1.fill_file(10, full_path)
+    t2.pseudonyms(full_path)
+
+
+if __name__ == "__main__":
+    files_in_folder("C:\\Users\\Zinovev.MI\\Desktop\\", "task6.txt")
 
