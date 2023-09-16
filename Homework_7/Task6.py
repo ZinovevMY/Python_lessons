e60918a0b3b680c925d786fd2482c4021765e75c
ZinovevMY <mehan@list.ru>
2023-09-16 15:49:33 +0300

    Done

--- Homework_7/Task6.py
+++ Homework_7/Task6.py
@@ -3,31 +3,36 @@
 # ✔ Отсутствие/наличие директории не должно вызывать ошибок в работе функции
 # (добавьте проверки).
 # ✔ Существующие файлы не должны удаляться/изменяться в случае совпадения имён.
-
+import random
 from pathlib import Path
 import Task1 as t1
 import Task2 as t2
 import Task3 as t3
-import Task4 as t4
 
 
-def files_in_folder(dir_name: str, file_name: str):
+def generate_file_name(length: int)-> str:
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+               's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    name = ""
+    for _ in range(length):
+        name += random.choice(letters)
+    name += ".txt"
+    return name
+
+
+def files_in_folder(dir_name: str):
     full_path = Path(dir_name)
     if not full_path.exists():
         Path.mkdir(full_path)
-    else:
-        cnt = 1
-        while full_path.exists():
-            stem = full_path.stem
-            new_stem = stem + str(cnt)
-            new_path = full_path.with_stem(new_stem)
-            full_path.rename(new_path)
-            cnt += 1
 
-    t1.fill_file(10, full_path)
-    t2.pseudonyms(full_path)
+    file1 = dir_name + generate_file_name(8)
+    file2 = dir_name + generate_file_name(8)
+    file3 = dir_name + generate_file_name(8)
+    t1.fill_file(10, file1)
+    t2.pseudonyms(file2)
+    t3.file_comparator(file1, file2, file3)
 
 
 if __name__ == "__main__":
-    files_in_folder("C:\\Users\\Zinovev.MI\\Desktop\\", "task6.txt")
+    files_in_folder("C:\\Разное\\Python_lessons\\Info\\")
 
