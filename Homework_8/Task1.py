6c0d1992694d149e17b7bcf6ec4778ef016023ff
ZinovevMY <mehan@list.ru>
2023-09-23 16:08:58 +0300

    Done

--- Homework_8/Task1.py
+++ Homework_8/Task1.py
@@ -8,6 +8,18 @@ import json
 
 
 def json_creator(file_path: str):
-    temp_dict = {}
     with open(file_path, encoding="utf-8") as file:
-        for line in file.readline():
+        temp_dict = {}
+        all_vals = file.read().split('\n')
+        for line in all_vals:
+            line = line.strip()
+            if line:
+                line = line.replace("  ", " ")
+                values = [val for val in line.split(" ")]
+                temp_dict[values[0].capitalize()] = float(values[1])
+    with open("task1.json", "w") as json_file:
+        json.dump(temp_dict, json_file)
+
+
+if __name__ == "__main__":
+    json_creator("C:\\Разное\\Python_lessons\\Homework_7\\task3_text.txt")
