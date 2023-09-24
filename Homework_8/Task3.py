5fd233971235e6328cca59f1b9b810a776d02773
ZinovevMY <mehan@list.ru>
2023-09-24 17:26:49 +0300

    Done

--- Homework_8/Task3.py
+++ Homework_8/Task3.py
@@ -7,14 +7,27 @@ def json_to_csv(source_path: str, dst_path: str):
     try:
         with open(source_path, encoding="utf-8") as json_file:
             data: dict = json.load(json_file)
-    except:
+
+    except FileNotFoundError:
         print("File not found!")
         exit(0)
+    csv_data = []
 
-    with open(dst_path, "w", newline='', encoding="utf-8") as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerow(data.keys())
-        writer.writerow(data.values())
+    for level, user in data.items():
+        for id, name in user.items():
+            csv_data.append({"level": level,
+                             "id": id,
+                             "name": name})
+    with open(dst_path, "w", newline='') as csv_file:
+        csv_writer = csv.DictWriter(csv_file, fieldnames=["level", "id", "name"])
+        csv_writer.writeheader()
+        csv_writer.writerows(csv_data)
+
+    # Вариант без DictWriter
+    # with open(dst_path, "w", newline='', encoding="utf-8") as csv_file:
+    #     writer = csv.writer(csv_file)
+    #     writer.writerow(data.keys())
+    #     writer.writerow(data.values())
 
 
 if __name__ == "__main__":
---
+++ Homework_8/Task4.py
@@ -0,0 +1,38 @@
+# Прочитайте созданный в прошлом задании csv файл без использования csv.DictReader.
+# Дополните id до 10 цифр незначащими нулями.
+# В именах первую букву сделайте прописной.
+# Добавьте поле хеш на основе имени и идентификатора.
+# Получившиеся записи сохраните в json файл, где каждая строка csv файла представлена как отдельный json словарь.
+# Имя исходного и конечного файлов передавайте как аргументы функции.
+import csv
+import hashlib
+import json
+
+
+def read_csv(src_path: str, dst_path: str):
+    try:
+        with open(src_path, encoding="utf-8") as file:
+            data = list(csv.reader(file))
+            del data[0]
+            for i in range(len(data)):
+                data[i][0] = data[i][0] + "0" * (10 - len(data[i][0]))
+                data[i][2] = data[i][2].capitalize()
+                hash_data = data[i][1] + data[i][2]
+                hash_obj = hashlib.md5(hash_data.encode())
+                data[i].append(hash_obj.hexdigest())
+        with open(dst_path, "a") as json_file:
+            json_dict: dict = {}
+            for i in range(len(data)):
+                key = data[i][0]
+                if key not in json_dict:
+                    json_dict[key] = {}
+                json_dict[key] = (data[i][1:])
+                json.dump(json_dict, json_file, indent=2)
+                json_dict.clear()
+    except FileNotFoundError:
+        print("File not found!")
+        exit(0)
+
+
+if __name__ == "__main__":
+    read_csv("task3.csv", "task4.json")
