649ebae4fb2dc864ce7e73187c7324a667cb2f1f
ZinovevMY <mehan@list.ru>
2023-09-28 14:07:58 +0300

    Done

--- Homework_8/Task7.py
+++ Homework_8/Task7.py
@@ -1,5 +1,16 @@
 # Прочитайте созданный в прошлом задании csv файл без использования csv.DictReader.
 # Распечатайте его как pickle строку.
+import csv
+import pickle
 
 
 def pickle_reader(file_path: str):
+    res = b''
+    with open(file_path) as file:
+        pickle_str = csv.reader(file)
+        for row in pickle_str:
+            res += pickle.dumps(row)
+        print(res)
+
+
+pickle_reader("task6.csv")
