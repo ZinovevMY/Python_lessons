f2602826d4d7fc94cf8bd43531a59834c7b0c2aa
ZinovevMY <mehan@list.ru>
2023-10-21 14:40:03 +0300

    Done

---
+++ Homework_10/Task2.py
@@ -0,0 +1,20 @@
+# Создайте класс прямоугольник.
+# Класс должен принимать длину и ширину при создании экземпляра.
+# У класса должно быть два метода, возвращающие периметр и площадь.
+# Если при создании экземпляра передаётся только одна сторона, считаем что у нас квадрат.
+
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def perimeter(self):
+        return (self.length + self.width) * 2
+
+    def square(self):
+        return self.length * self.width
+
+
+rect = Rectangle(5, 12)
+print(rect.perimeter())
+print(rect.square())
