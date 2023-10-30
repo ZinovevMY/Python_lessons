12a734802c84a7a4bb78dbcffd63cac10f932676
ZinovevMY <mehan@list.ru>
2023-10-30 15:01:25 +0300

    Done

---
+++ Homework_11/Task1.py
@@ -0,0 +1,18 @@
+# Создайте класс Моя Строка, где:
+# будут доступны все возможности str
+# дополнительно хранятся имя автора строки и время создания (time.time)
+import time
+
+
+class MyStr(str):
+    def __new__(cls, string: str, author_name: str):
+        instance = super().__new__(cls, string)
+        instance.author_name = author_name
+        instance.create_time = time.time()
+        return instance
+
+
+if __name__ == "__main__":
+    new_str = MyStr('Мама мыла раму', 'Я')
+    print(new_str)
+    print(new_str.author_name, new_str.create_time)
