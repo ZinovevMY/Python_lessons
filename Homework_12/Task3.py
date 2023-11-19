c28b917513acad34dc75719ab25c7a68dd394b5d
ZinovevMY <mehan@list.ru>
2023-11-19 16:04:26 +0300

    Done

---
+++ Homework_12/Task3.py
@@ -0,0 +1,73 @@
+# Доработайте класс прямоугольник из прошлых семинаров.
+# Добавьте возможность изменять длину и ширину прямоугольника и встройте контроль недопустимых значений (отрицательных).
+# Используйте декораторы свойств.
+
+class Rectangle:
+    def __init__(self, length, width=None):
+        self._length = length
+        if width is None:
+            self._width = self._length
+        else:
+            self._width = width
+
+    @property
+    def length(self):
+        return self._length
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, width):
+        if width > 0:
+            self._width = width
+        else:
+            raise ValueError("Ширина не может быть отрицательной!")
+
+    @length.setter
+    def length(self, length):
+        if length > 0:
+            self._length = length
+        else:
+            raise ValueError("Длина не может быть отрицательной!")
+
+    def perimeter(self):
+        return (self.length + self.width) * 2
+
+    def square(self):
+        return self.length * self.width
+
+    def __add__(self, other):
+        return Rectangle(self.length, self.width + other.width)
+
+    def __sub__(self, other):
+        return Rectangle(self.length, abs(self.width - other.width))
+
+    def __eq__(self, other):
+        return self.square() == other.square()
+
+    def __ne__(self, other):
+        return self.square() != other.square()
+
+    def __gt__(self, other):
+        return self.square() > other.square()
+
+    def __ge__(self, other):
+        return self.square() <= other.square()
+
+    def __lt__(self, other):
+        return self.square() < other.square()
+
+    def __le__(self, other):
+        return self.square() >= other.square()
+
+
+if __name__ == '__main__':
+    rect1 = Rectangle(5, 10)
+    rect2 = Rectangle(6, 7)
+    print(rect1.width)
+    print(rect1.square())
+    rect1.length = 8
+    print(rect1.square())
+    # rect1.width = -3
