4345b1ed9b31361efed7a56e7c66eae40f042454
ZinovevMY <mehan@list.ru>
2023-11-19 15:04:56 +0300

    Done

--- Homework_12/Task1.py
+++ Homework_12/Task1.py
@@ -2,26 +2,37 @@
 # Экземпляр должен запоминать последние k значений.
 # Параметр k передаётся при создании экземпляра.
 # Добавьте метод для просмотра ранее вызываемых значений и их факториалов.
-from collections import defaultdict
+# Создайте менеджер контекста, который при выходе сохраняет значения в JSON файл.
+import json
 from math import factorial
 
 
 class Factorial:
     def __init__(self, count):
-        self.storage = defaultdict(list)
+        self.storage = []
         self.count = count
 
     def __call__(self, value):
         self.result = factorial(value)
+        if len(self.storage) == self.count:
+            self.storage.pop(0)
         if len(self.storage) < self.count:
-            self.storage[value].append(self.result)
+            self.storage.append({value: self.result})
 
     def __str__(self):
         return f'Последние {self.count} значений: {self.storage}'
 
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        with open("task1.json", "w") as file:
+            json.dump(self.storage, file)
+
 
 if __name__ == '__main__':
-    f = Factorial(6)
-    for i in range(4, 10):
-        f(i)
+    fact = Factorial(5)
+    with fact as f:
+        for i in range(4, 10):
+            f(i)
     print(f)
